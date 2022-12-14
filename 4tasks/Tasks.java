b40737bab830f2313a2156c49a895aea6e4831a3
EgorKirnosov <kirnosov.eg@yandex.ru>
2022-12-14 23:51:04 +0300

    Доделал 4 таски

--- 4tasks/Tasks.java
+++ 4tasks/Tasks.java
@@ -1,4 +1,3 @@
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 class Task {
@@ -7,10 +6,16 @@ class Task {
         // clasters("((())())(()(()()))");
         // toCamelCase("is_modal_open");
         // toSnakeCase("getColor");
-        overTime(new float[] { 16, 18, 30, 1.8f });
+        // overTime(new float[] { 13.25f, 15, 30, 1.5f });
+        // BMI("154 pounds", "2 meters");
+        // bugger(4);
+        // toStarShorthand("77777geff");
+        // System.out.println(doesRhyme("You are off to the races", "a splendid day."));
+        // System.out.println(trouble(666789, 12345667));
+        // System.out.println(countUniqueBooks("ZZABCDEF", 'Z'));
     }
 
-    public static void bessi(int n, int k, String source) {
+    public static ArrayList<String> bessi(int n, int k, String source) {
         var words = source.split(" ");
         var lines = new ArrayList<String>();
         var currentLine = new StringBuilder();
@@ -36,11 +41,10 @@ class Task {
             currentLine.append(word);
         }
         lines.add(currentLine.toString());
-        for (var line : lines)
-            System.out.println(line);
+        return lines;
     }
 
-    public static void clasters(String source) {
+    public static ArrayList<String> clasters(String source) {
         var groups = new ArrayList<String>();
         var builder = new StringBuilder();
         var depth = 0;
@@ -57,15 +61,12 @@ class Task {
                 depth = 0;
             }
         }
-        for (var group : groups) {
-            System.out.println(group);
-        }
+        return groups;
     }
 
-    public static void toCamelCase(String source) {
+    public static String toCamelCase(String source) {
         if (source.indexOf("_") == -1) {
-            System.out.println(source);
-            return;
+            return source;
         }
 
         var builder = new StringBuilder();
@@ -81,10 +82,10 @@ class Task {
                 builder.append(symbol);
             }
         }
-        System.out.println(builder.toString());
+        return builder.toString();
     }
 
-    public static void toSnakeCase(String source) {
+    public static String toSnakeCase(String source) {
         var builder = new StringBuilder();
         for (var i = 0; i < source.length(); i++) {
             var symbol = source.charAt(i);
@@ -95,12 +96,12 @@ class Task {
                 builder.append(symbol);
             }
         }
-        System.out.println(builder.toString());
+        return builder.toString();
     }
 
-    public static void overTime(float[] inputs) {
+    public static String overTime(float[] inputs) throws Exception {
         if (inputs.length != 4)
-            return;
+            throw new Exception("Неправильный ввод");
         var startTime = inputs[0];
         var endTime = inputs[1];
         var salary = inputs[2];
@@ -124,6 +125,116 @@ class Task {
         else
             answer += (endTime % 1f) * salary;
 
-        System.out.println(String.format("$%.2f", answer));
+        return String.format("$%.2f", answer);
+    }
+
+    public static String BMI(String weight, String height) {
+        final float POUNDS_TO_KILOS = 0.45f;
+        final float INCHES_TO_METERS = 0.025f;
+        var weightParams = weight.split(" ");
+        var heightParams = height.split(" ");
+
+        var weightKilos = weightParams[1].equals("pounds") ? Float.parseFloat(weightParams[0]) * POUNDS_TO_KILOS
+                : Float.parseFloat(weightParams[0]);
+
+        var heightMeters = heightParams[1].equals("inches") ? Float.parseFloat(heightParams[0]) * INCHES_TO_METERS
+                : Float.parseFloat(heightParams[0]);
+
+        var bmi = weightKilos / Math.pow(heightMeters, 2);
+        var status = bmi < 18.5f ? "Underweight" : bmi < 25f ? "Normal weight" : "Overweight";
+
+        var answer = String.format("%s %s", bmi, status);
+        return answer;
+    }
+
+    public static int bugger(int num) {
+        var iteration = 0;
+        while (num / 10 > 0) {
+            iteration++;
+            var mult = 1;
+            while (num != 0) {
+                var digit = num % 10;
+                num -= digit;
+                num /= 10;
+                mult *= digit;
+            }
+            num = mult;
+        }
+        return iteration;
+    }
+
+    public static String toStarShorthand(String input) {
+        var lastSymbol = input.charAt(0);
+        var length = 0;
+        var builder = new StringBuilder();
+        for (var symbol : input.toCharArray()) {
+            if (symbol == lastSymbol)
+                length++;
+            else {
+                if (length > 1)
+                    builder.append(String.format("%s*%s", lastSymbol, length));
+                else
+                    builder.append(lastSymbol);
+                length = 1;
+                lastSymbol = symbol;
+            }
+        }
+        if (length > 1)
+            builder.append(String.format("%s*%s", lastSymbol, length));
+        else
+            builder.append(lastSymbol);
+
+        return builder.toString();
+    }
+
+    public static boolean doesRhyme(String a, String b) {
+        var aWords = a.split("");
+        var aLastWord = aWords[aWords.length - 1].toLowerCase();
+
+        var bWords = b.split("");
+        var bLastWord = bWords[bWords.length - 1].toLowerCase();
+
+        for (char symbol : bLastWord.toCharArray()) {
+            if ("aeiou".indexOf(symbol) == -1)
+                continue;
+            if (aLastWord.indexOf(symbol) == -1)
+                return false;
+        }
+        for (char symbol : aLastWord.toCharArray()) {
+            if ("aeiou".indexOf(symbol) == -1)
+                continue;
+            if (bLastWord.indexOf(symbol) == -1)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static boolean trouble(long a, long b) {
+        var aString = Long.toString(a);
+        var bString = Long.toString(b);
+
+        for (int i = 0; i < 10; i++) {
+            var seqB = String.format("%s%s", i, i);
+            var seqA = String.format("%s%s%s", i, i, i);
+            if (aString.contains(seqA) && bString.contains(seqB))
+                return true;
+        }
+        return false;
+    }
+
+    public static int countUniqueBooks(String input, char separator) {
+        var uniqueBooks = new ArrayList<Character>();
+        var started = false;
+        for (var book : input.toCharArray()) {
+            if (started) {
+                if (book == separator)
+                    started = false;
+                else if (!uniqueBooks.contains(book))
+                    uniqueBooks.add(book);
+            } else if (book == separator)
+                started = true;
+        }
+        return uniqueBooks.size();
     }
 }
